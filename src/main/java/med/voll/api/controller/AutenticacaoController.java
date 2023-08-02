71879450604c4498ebd36a31aec0941e0f75b876
devCarlosAlexandre <carloalexandresousa@gmail.com>
2023-08-02 17:02:40 -0300

    implementado geração de token jwt

--- src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -3,6 +3,9 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.AutenticacaoService;
 import med.voll.api.domain.usuario.DadosAutenticacao;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.DadosTokenJWT;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,11 +22,15 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
-    @PostMapping
-    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-       var autencation = manager.authenticate(token);
+    @Autowired
+    private TokenService tokenService;
 
-       return ResponseEntity.ok().build();
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
+        var Autenticationtoken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var autencation = manager.authenticate(Autenticationtoken);
+        var tokenJWT = tokenService.gerarToken((Usuario) autencation.getPrincipal());
+
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
     }
 }
